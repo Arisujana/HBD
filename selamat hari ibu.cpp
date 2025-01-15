60afb78636d5ebb7a410d1195b809c2002257745
Arisujana <154985204+Arisujana@users.noreply.github.com>
2025-01-15 22:09:17 +0800

    Add files via upload

---
+++ selamat hari ibu.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    std::cout << "Selamat Hari Ibu! Terima kasih atas segala kasih sayang dan dedikasi, Ibu tercinta.\n";
+    return 0;
+}
